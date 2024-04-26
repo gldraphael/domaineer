ab8d2853f40aed2d4165043976f7b532aafca7ad
Galdin Raphael <gldraphael@users.noreply.github.com>
2024-04-26 11:36:50 +0530

    Add basic domain parsing logic

---
+++ tests/Domaineer.Core.Tests/DomainParserTests/GetGtld_should.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+
+namespace Domaineer.Core.Tests.DomainParserTests;
+
+public class GetGtld_should
+{
+
+    [Theory]
+    [InlineData("galdin.dev", "dev")]
+    // [InlineData("google.co.in", "co.in")]
+    [InlineData("facebook.com", "com")]
+    public void Return_gtld(string domain, string gtld)
+    {
+        var result = DomainParser.GetGtld(domain);
+
+        result.IsSuccess
+            .Should().BeTrue();
+
+        result.Value
+            .Should().BeEquivalentTo(gtld);
+    }
+
+    [Fact]
+    public void Fail_if_invalid_domain()
+    {
+        var result = DomainParser.GetGtld(".blah.com.");
+
+        result.IsSuccess
+            .Should().BeFalse();
+    }    
+}
---
+++ tests/Domaineer.Core.Tests/DomainParserTests/IsValid_should.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+namespace Domaineer.Core.Tests.DomainParserTests;
+
+public class IsValid_should
+{
+    [Theory]
+    [InlineData("galdin.dev")]
+    [InlineData("galdin42.dev")]
+    public void Return_true_if_domain_is_valid(string domain)
+    {
+        DomainParser.IsValid(domain)
+            .Should().BeTrue();
+    }
+
+    [Theory]
+    // > 255 characters
+    [InlineData("galdin-liomsd2zfhro0klep5tz1axgr3ccxqcv64wxqci84vyzng3ozjtuobg5cbwrb70kve701ulopzpda4bjuperl2pzfqvjpronhxjckwe45v6ddaftliks2thygtiznhteewxpocuyzsi0fuo89td38qb8tstkwfucstw2hstzipinm4l7chr0ccntrqavjvdxyjedfoexzepteehpwteejfqrrcbldggpufaucdbsy2khjkwhg33ciptqi0yqvazt.dev")]
+    public void Return_false_if_domain_is_too_long(string domain)
+    {
+        DomainParser.IsValid(domain)
+            .Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData(".galdin.dev")] // start with a .
+    [InlineData("galdin.dev.")] // end with a .
+    // [InlineData("gal@din.dev")]
+    public void Return_false_if_domain_contains_invalid_characters(string domain)
+    {
+        DomainParser.IsValid(domain)
+            .Should().BeFalse();
+    }
+}
