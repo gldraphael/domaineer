00f2c87207c4ba03c5ea0049362c5b85feb75262
Galdin Raphael <gldraphael@users.noreply.github.com>
2024-05-07 00:43:05 +0530

    Get it to work

---
+++ src/Domaineer.WebApp/Services/DogeService.cs
@@ -0,0 +1,34 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Domaineer.WebApp.Services;
+
+public class DogeService
+{
+    const string DOGE_PATH = "doge";
+
+    public static async Task<bool> DoesDomainExist(string domain)
+    {
+        var result = await Cli.Wrap(DOGE_PATH)
+            .WithArguments(["--json", domain])
+            .ExecuteBufferedAsync();
+        if (result.IsSuccess is false) return false;
+
+        var response = JsonSerializer.Deserialize<DogeResponse>(result.StandardOutput);
+        if (response is null) return false;
+
+        return !response.Responses.Any(r => r.Error is not null && r.Error == "NXDomain");
+    }
+}
+
+
+public record Response(
+    [property: JsonPropertyName("error")] string? Error
+);
+
+public record DogeResponse(
+    [property: JsonPropertyName("responses")] IReadOnlyList<Response> Responses
+);
+
